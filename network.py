2666ab5cd17d55cf027b98d540321ba51f4f5f0d
tuesdaybornwhale <tuesdaybornwhale@gmail.com>
2023-08-30 20:24:58 +0200

    feedforward

--- network.py
+++ network.py
@@ -17,11 +17,16 @@ Class Network():
         self.biases = [np.random.randn(y,1) for y in sizes]
 
         # the following code generates a list of length num_layers which in itself contains a matrix representing the weights between the 
-        # neurons in the ith and the jth network. self.weights[i][j][k] conatins the connections between the kth neuron in the ith layer and
+        # neurons in the ith and the jth network. self.weights[i][j][k] contains the connections between the kth neuron in the ith layer and
         # the jth neuron in the (i+1)th layer.
         self.weights = [np.random.randn(y,x) for y, x in zip(sizes[1:], sizes[:-1])]
 
-    def feedforward(x, y):
+    def feedforward(self, x):
         """This method feeds a training example x, which is a vector of length sizes[0], through the entire network and outputs the
         activations in the final network."""
-        
+        for i in range(num_layers):
+            # vectorized function returns a vector of activations for the next layer
+            x = sigmoid(np.dot(x, self.weights[i][j]) - self.biases[i]) 
+        return x
+    
+    def cost(self, x, y)
