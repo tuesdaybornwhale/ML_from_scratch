9276295b54181881b6237b2d5fc9f117422e4f29
tuesdaybornwhale <tuesdaybornwhale@gmail.com>
2023-08-31 20:33:58 +0200

    created main code and initiated a network object

--- image_classifier.py
+++ image_classifier.py
@@ -6,5 +6,14 @@ Desc: Image classifier for handwritten digits.
 
 import random
 import numpy as np
+from network import *
 
+
+def main():
+    """main executable code."""
+    # the size of the first layer (input layer) and last (output) layer are determined by the type of data we're receiving (784 greyscale 
+    # pixels) and the type of data we want to output (we can only predict among ten digits). Our only degrees of freedom are the amount of
+    # hidden layers and the size of the layer(s) 
+    sizes = [784, 30, 10]
+    net = Network(sizes)
 
