9003a337889b8309f801f5b7e99404608a10e93a
tuesdaybornwhale <tuesdaybornwhale@gmail.com>
2023-08-31 20:39:10 +0200

    updated readme to reflect project progress

--- Readme.md
+++ Readme.md
@@ -1,4 +1,6 @@
-This repo seeks to build some simple ML projects from scratch using no ML libraries
+This repo seeks to build some simple ML projects from scratch using no ML libraries. The only project on here so far is a simple image classifier for MNIST data.
 
 The code in the image classifier project is largely inspired by the code found on 
 https://github.com/mnielsen/neural-networks-and-deep-learning.
+
+update 2023-08-31 the code for the class network is more or less sketched (still not debugged). The major piece missing is the loading of MNIST data itself.
--- image_classifier.py
+++ image_classifier.py
@@ -11,9 +11,9 @@ from network import *
 
 def main():
     """main executable code."""
-    # the size of the first layer (input layer) and last (output) layer are determined by the type of data we're receiving (784 greyscale 
+    # the size of the first layer (input layer) and last (output) layer are determined by the type of data we're receiving (28**2 greyscale 
     # pixels) and the type of data we want to output (we can only predict among ten digits). Our only degrees of freedom are the amount of
     # hidden layers and the size of the layer(s) 
-    sizes = [784, 30, 10]
+    sizes = [28**2, 30, 10]
     net = Network(sizes)
 
